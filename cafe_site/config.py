8772eb9176036f3adaa9f8355e00bcaa983384b0
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-11-20 19:15:30 +0900

    Update config.py

--- cafe_site/config.py
+++ cafe_site/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'postgres://qplnpvmmngeqvw:e19efe958cba48a0a58f851f161563c308868f66324dc51d52dfce6306deedef@ec2-34-233-226-84.compute-1.amazonaws.com:5432/d9gn3jv3r0o81u'
+SQLALCHEMY_DATABASE_URI = ''
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
 SECRET_KEY = 'secret key'
