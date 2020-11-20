72b68382c6b9ca18b570f6df6ce28fb4dc47d657
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-11-20 19:10:24 +0900

    Update config.py

--- cafe_site/config.py
+++ cafe_site/config.py
@@ -2,7 +2,7 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///cafe_site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 =======
-SQLALCHEMY_DATABASE_URI = 'postgres://qplnpvmmngeqvw:e19efe958cba48a0a58f851f161563c308868f66324dc51d52dfce6306deedef@ec2-34-233-226-84.compute-1.amazonaws.com:5432/d9gn3jv3r0o81u'
+SQLALCHEMY_DATABASE_URI = 'hobby-devのPasswordを入力'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 >>>>>>> future/database
 DEBUG = True
