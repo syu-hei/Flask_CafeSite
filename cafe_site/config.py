750d664b516b59f2e90dc6f5bced3aabfeb085ed
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-12-09 23:57:25 +0900

    Update config.py

--- cafe_site/config.py
+++ cafe_site/config.py
@@ -2,7 +2,7 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///cafe_site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 =======
-SQLALCHEMY_DATABASE_URI = 'hobby-devのPasswordを入力'
+SQLALCHEMY_DATABASE_URI = 'hobby-devのURIを入力'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 >>>>>>> future/database
 DEBUG = True
